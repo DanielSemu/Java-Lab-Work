9814254b1fa1fffd4d0ff7bf3a76182312c6f51a
DanielSemu <danielsemu06@gmail.com>
2021-12-27 11:23:44 +0300

    login page

---
+++ src/notepad/NewPage.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package notepad;
+
+/**
+ *
+ * @author Daniel
+ */
+import javax.swing.*;  
+import java.awt.*;  
+  
+//create NewPage class to create a new page on which user will navigate  
+class NewPage extends JFrame  
+{  
+    //constructor  
+    NewPage()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Welcome");  
+        setSize(400, 200);  
+    }  
+}  
